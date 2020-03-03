81c9cd1f5f58ebd09e55b45ddf2e97c1402a6365
TheRevanchist <DarthRevan19>
2020-03-03 19:36:55 +0100

    Add new changes

--- net/__init__.py
+++ net/__init__.py
@@ -1,3 +1,6 @@
 from .embed import embed
 from .inception_bn import bn_inception, Inception_embed#, bn_inception_augmented
-from .resnet import resnet50
+from .resnet import resnet18, resnet34, resnet50, resnet101, resnet152
+from .densenet import densenet121, densenet161, densenet169, densenet201
+from .shufflenet import shufflenet_v2_x0_5, shufflenet_v2_x1_0, shufflenet_v2_x1_5, shufflenet_v2_x2_0
+from .vgg import vgg16_bn, vgg16
