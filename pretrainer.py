8c155d19a0df18ceffbee6210e089a78b54b18c4
JennySeidenschwarz <jenny.seidenschwarz@web.de>
2020-05-08 12:24:59 -0700

    adapted pretrainer

--- pretrainer.py
+++ pretrainer.py
@@ -20,60 +20,6 @@ import random
 from RAdam import RAdam
 
 
-class Hyperparameters():
-    def __init__(self, dataset_name='cub'):
-        self.dataset_name = dataset_name
-        if dataset_name == 'cub':
-            self.dataset_path = '../../datasets/CUB_200_2011'
-        elif dataset_name == 'cars':
-            self.dataset_path = '../../datasets/CARS'
-        elif dataset_name == 'Market':
-            self.dataset_path = '../../datasets/Market'
-        elif dataset_name == 'cuhk03':
-            self.dataset_path = '../../datasets/cuhk03'
-        else:
-            self.dataset_path = '../../datasets/Stanford'
-
-        self.num_classes = {'cub': 100, 'cars': 98, 'Stanford': 11318, 'Market': 651, 'cuhk03': 1267}
-        self.num_classes_iteration = {'cub': 6, 'cars': 5, 'Stanford': 10, 'Market': 5, 'cuhk03': 5}
-        self.num_elemens_class = {'cub': 9, 'cars': 7, 'Stanford': 6, 'Market': 7, 'cuhk03': 7}
-        self.get_num_labeled_class = {'cub': 2, 'cars': 3, 'Stanford': 2, 'Market': 2, 'cuhk03': 2}
-        # self.learning_rate = 0.0002
-        self.learning_rate = {'cub': 0.0001563663718906821, 'cars': 0.0002, 'Stanford': 0.0006077651100709081, 'Market': 0.0002, 'cuhk03': 0.00002}
-        self.weight_decay = {'cub': 6.059722614369727e-06, 'cars': 4.863656728256105e-07, 'Stanford': 5.2724883734490575e-12, 'Market': 4.863656728256105e-07, 'cuhk03': 4.863656728256105e-07}
-        self.softmax_temperature = {'cub': 24, 'cars': 79, 'Stanford': 54, 'Market': 79, 'cuhk03': 79}
-
-    def get_path(self):
-        return self.dataset_path
-
-    def get_number_classes(self):
-        return self.num_classes[self.dataset_name]
-
-    def get_number_classes_iteration(self):
-        return self.num_classes_iteration[self.dataset_name]
-
-    def get_number_elements_class(self):
-        return self.num_elemens_class[self.dataset_name]
-
-    def get_number_labeled_elements_class(self):
-        return self.get_num_labeled_class[self.dataset_name]
-
-    def get_learning_rate(self):
-        return self.learning_rate[self.dataset_name]
-
-    def get_weight_decay(self):
-        return self.weight_decay[self.dataset_name]
-
-    def get_epochs(self):
-        return 70
-
-    def get_num_gtg_iterations(self):
-        return 1
-
-    def get_softmax_temperature(self):
-        return self.softmax_temperature[self.dataset_name]
-
-
 class DataSet(torch.utils.data.Dataset):
     def __init__(self, root, labels, file_names, transform=None):
         # e.g., labels = range(0, 50) for using first 50 classes only
@@ -108,78 +54,11 @@ def init_args():
                         help='If apex should be used')
     parser.add_argument('--oversampling', default=1, type=int,
                         help='If oversampling shoulf be used')
-    parser.add_argument('--manually_lr_decay', default=0, type=int,
-                        help='If manually lr decay should be applied')
     parser.add_argument('--model_name', default='resnet', type=str,
                         help='which model shoul be fine tuned')
     parser.add_argument('--feature_extract', default=0, type=int,
                         help='If only last layer should be updated')
     parser.add_argument('--num_epochs', default=10, type=int)
-    parser.add_argument('--dataset_name', default=dataset_name, type=str,
-                        help='The name of the dataset')
-    parser.add_argument('--cub-root', default=hyperparams.get_path(),
-                        help='Path to dataset folder')
-    parser.add_argument('--cub-is-extracted', action='store_true',
-                        default=True,
-                        help='If `images.tgz` was already extracted, do not extract it again.' +
-                             ' Otherwise use extracted data.')
-    parser.add_argument('--embedding-size', default=512, type=int,
-                        dest='sz_embedding', help='The embedding size')
-    parser.add_argument('--nb_classes',
-                        default=hyperparams.get_number_classes(), type=int,
-                        help='Number of first [0, N] classes used for training and ' +
-                             'next [N, N * 2] classes used for evaluating with max(N) = 100.')
-    '''parser.add_argument('--num_classes_iter',
-                        default=hyperparams.get_number_classes_iteration(),
-                        type=int,
-                        help='Number of classes in the minibatch')
-    parser.add_argument('--num_elements_class',
-                        default=hyperparams.get_number_elements_class(),
-                        type=int,
-                        help='Number of samples per each class')
-    parser.add_argument('--num_labeled_points_class',
-                        default=hyperparams.get_number_labeled_elements_class(),
-                        type=int,
-                        help='Number of labeled samples per each class')
-    parser.add_argument('--lr-net', default=hyperparams.get_learning_rate(),
-                        type=float, help='The learning rate')
-    parser.add_argument('--weight-decay',
-                        default=hyperparams.get_weight_decay(), type=float,
-                        help='The l2 regularization strength')
-    parser.add_argument('--nb_epochs', default=hyperparams.get_epochs(),
-                        type=int, help='Number of training epochs.')'''
-    parser.add_argument('--nb_workers', default=4, type=int,
-                        help='Number of workers for dataloader.')
-    parser.add_argument('--net_type', default='bn_inception', type=str,
-                        choices=['bn_inception', 'densenet121', 'densenet161',
-                                 'densenet169', 'densenet201',
-                                 'resnet18', 'resnet34', 'resenet50',
-                                 'resnet101', 'resnet152'],
-                        help='The type of net we want to use')
-    parser.add_argument('--sim_type', default='correlation', type=str,
-                        help='type of similarity we want to use')
-    parser.add_argument('--set_negative', default='hard', type=str,
-                        help='type of threshold we want to do'
-                             'hard - put all negative similarities to 0'
-                             'soft - subtract minus (-) negative from each entry')
-    parser.add_argument('--num_iter_gtg',
-                        default=hyperparams.get_num_gtg_iterations(), type=int,
-                        help='Number of iterations we want to do for GTG')
-    parser.add_argument('--embed', default=0, type=int,
-                        help='boolean controling if we want to do embedding or not')
-    parser.add_argument('--scaling_loss', default=1, type=int,
-                        dest='scaling_loss',
-                        help='Scaling parameter for the loss')
-    '''parser.add_argument('--temperature',
-                        default=hyperparams.get_softmax_temperature(),
-                        help='Temperature parameter for the softmax')'''
-    parser.add_argument('--decrease_learning_rate', default=10., type=float,
-                        help='Number to divide the learnign rate with')
-    parser.add_argument('--id', default=1, type=int,
-                        help='id, in case you run multiple independent nets, for example if you want an ensemble of nets')
-    parser.add_argument('--is_apex', default=0, type=int,
-                        help='if 1 use apex to do mixed precision training')
-
 
     return parser.parse_args()
 
@@ -201,7 +80,7 @@ class PreTrainer():
 
         model, input_size, params_to_update = self.get_model(num_classes)
 
-        optimizer = RAdam([{'params': params_to_update, 'lr': config['lr']}], weight_decay=config['weight_decay'])
+        optimizer = RAdam([{'params': params_to_update, 'lr': config['lr']}])
 
         if self.args.apex_on:
             model, optimizer = amp.initialize(model, optimizer, opt_level="O1")
@@ -516,39 +395,15 @@ def main():
 
     trainer = PreTrainer(args, data_dir, save_name, device)
 
-    best_acc = 0
-    num_iter = 100
-    # Random search
-    for i in range(num_iter):
+    config = {'lr': 0.001,
+              'batch_size': 64}
 
-        # random search for hyperparameters
-        lr = 10**random.uniform(-8, -3)
-        batch_size = random.choice([8, 16, 32, 64])
-        weight_decay = 10 ** random.uniform(-15, -6)
+    model, val_acc_history, model = trainer.train_model(config)
 
-        config = {'lr': lr,
-                  'weight_decay': weight_decay,
-                  'batch_size': batch_size}
+    if args.model_name == 'resnet':
+        args.model_name = 'resnet50'
 
-        model, val_acc_history = trainer.train_model(config)
-
-        hypers = '_'.join([str(k) + '_' + str(v) for k, v in config.items()])
-
-        acc = max(val_acc_history)
-        if acc > best_acc:
-            best_model = model
-            best_acc = acc
-            best_hypers = hypers
-
-        save_name = os.path.join('search_results', str(acc) + hypers + '.txt')
-        with open(save_name, 'w') as file:
-            for e in val_acc_history:
-                file.write(str(e.data))
-                file.write('\n')
-
-    torch.save(best_model, 'fine_tuned' + args.model_name + args.dataset_name + '.pth')
-
-    print("Best Hyperparameters found: " + best_hypers)
+    torch.save(model, 'finetuned_' + args.dataset_name + '_' + args.model_name + '.pth')
 
 
 
