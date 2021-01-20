990fe6c14a03a70b3f57f619f628a785725cb3d9
JennySeideschwarz <jenny.seidenschwarz@web.de>
2021-01-20 15:15:07 +0100

    clean up

--- dataset/__init__.py
+++
@@ -1,5 +0,0 @@
-
-from .cub import Birds, Birds_DML
-from .load_data import load_data
-from .extract_market import marketlike
-from .extract_cuhk import cuhk03
--- utils/utils.py
+++
@@ -1,7 +0,0 @@
-import os
-import os.path as osp
-
-
-def make_dir(dir):
-    if not osp.isdir(dir):
-        os.makedirs(dir)
