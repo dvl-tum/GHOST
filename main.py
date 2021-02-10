1bb9ad8a8feb2ecf7e4d49acb3c97b994580b2a6
JennySeideschwarz <jenny.seidenschwarz@web.de>
2021-02-10 11:13:15 +0100

    rm log file and add distinction between config files

--- main.py
+++ main.py
@@ -7,10 +7,13 @@ import time
 import os.path as osp
 import os
 from src.manager import Manager
+from src.reid_manager import ManagerReID
 
-logger = logging.getLogger('Tracker')
+
+logger = logging.getLogger('AllReIDTracker')
 logger.setLevel(logging.INFO)
-formatter = logging.Formatter('%(asctime)s - %(message)s')
+#formatter = logging.Formatter('%(asctime)s - %(message)s')
+formatter = logging.Formatter('%(message)s')
 
 ch = logging.StreamHandler()
 ch.setLevel(logging.DEBUG)
@@ -24,7 +27,8 @@ warnings.filterwarnings("ignore")
 def init_args():
     parser = argparse.ArgumentParser(description='AllReID tracker')
     parser.add_argument('--config_path', type=str,
-                        default='config/config.yaml',
+                        #default='config/config_tracker.yaml',
+                        default='config/config_reid.yaml',
                         help='Path to config file')
 
     return parser.parse_args()
@@ -36,11 +40,16 @@ def main(args):
 
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
     logger.info('Switching to device {}'.format(device))
-
-    manager = Manager(device, time.time(), config['dataset'],
+    logger.info(config) 
+    if not 'reid.yaml' in args.config_path.split('_'):
+        manager = Manager(device, time.time(), config['dataset'],
+                          config['reid_net'], config['tracker'], train=True)
+        manager.train()
+        #manager._evaluate(mode='test')
+    else:
+        manager = ManagerReID(device, time.time(), config['dataset'],
                       config['reid_net'], config['tracker'])
-    manager.train()
-
+        manager.train()
 
 if __name__ == '__main__':
     args = init_args()
